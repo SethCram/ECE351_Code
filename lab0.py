a5b8e9db650c79e678c478cf99a3518cd452d912
Seth <86444777+SethCram@users.noreply.github.com>
2022-01-18 20:41:08 -0800

    Lab 0 only contained basic import statements and a header.

---
+++ lab0.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 18 20:09:53 2022
+
+@author: crazy
+"""
+################################################################
+# #
+# Seth Cram #
+# ECE351 Section 53 #
+# Lab Number 0 #
+# 1/25/2022 #
+# Any other necessary information needed to navigate the file #
+# 
+#   What did you do over last summer, or what are you planning to do this upcoming summer?
+#       Over last summer I spent most of my time with my family, taught myself how to use 
+#       the Unity Game Engine, C# and the basics of 3d modeling in Blender.     
+#
+#   What do you personally want to get out of this lab?
+#       I want improved coding skills in Python considering how widely used it is in 
+#       industry. 
+#
+################################################################
+
+import numpy
+import matplotlib.pyplot
+import scipy
+import control
+import pandas
